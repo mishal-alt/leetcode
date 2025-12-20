11272f400b0c5bb9df7c2929c204807ce8d986f2
Mishal KV <mishalkv.webdev@gmail.com>
2025-12-20 10:24:58 +0530

    Time: 0 ms (100%), Space: 56.8 MB (51.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  let n = nums.length;
+  let expected = (n * (n + 1)) / 2;
+  let actual = nums.reduce((a, b) => a + b, 0);
+  return expected - actual;
+};
