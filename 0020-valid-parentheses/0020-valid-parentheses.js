bc6273fc9121ab32848185315f7f40bbc06a37b8
Mishal KV <mishalkv.webdev@gmail.com>
2026-02-04 12:13:28 +0530

    Time: 4 ms (51.42%), Space: 55.5 MB (67.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stack = []
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '[' || ch === '{') {
+            stack.push(ch)
+        } else {
+            if (stack.length === 0) return false;
+            const top = stack.pop()
+            if(top!==map[ch]) return false
+        }
+       
+    }
+    return stack.length===0
+};
