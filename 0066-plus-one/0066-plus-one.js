f8b6c1c2c780a1e73cd59f2ca976acc834eebd0d
Mishal KV <mishalkv.webdev@gmail.com>
2026-02-07 13:24:05 +0530

    Time: 50 ms (1.83%), Space: 55.6 MB (5.18%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -2,10 +2,16 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-  let num = digits.join("");
-  let newnum = BigInt(num);
-  newnum++;
-  let numArr = newnum.toString().split("").map(Number);
-  return numArr;
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+
+  
+  digits.unshift(1);
+  return digits;
 };
