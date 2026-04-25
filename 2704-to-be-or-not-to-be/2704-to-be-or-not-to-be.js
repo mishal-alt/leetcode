cb8841c1430e356e439d68b32b6cd25b837ac804
Mishal KV <mishalkv.webdev@gmail.com>
2026-04-25 15:30:38 +0530

    Time: 56 ms (6.5%), Space: 54.1 MB (18.07%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -2,19 +2,24 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-  return {
-    toBe: function(expected) {
-      if (val === expected) return true;
-      throw new Error("Not Equal");
-    },
-    notToBe: function(expected) {
-      if (val !== expected) return true;
-      throw new Error("Equal");
+var expect = function (val) {
+    return {
+        toBe: (value) => {
+            if (val === value) {
+                return true
+            } else {
+                throw 'Not Equal'
+            }
+        },
+        notToBe: (value) => {
+            if (val !== value) {
+                return true
+            } else {
+                throw 'Equal'
+            }
+        }
     }
-  };
 };
-
 
 /**
  * expect(5).toBe(5); // true
