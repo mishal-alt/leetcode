391614b53340cce5996baea31cdddb8219b44890
Mishal KV <mishalkv.webdev@gmail.com>
2026-04-27 12:45:13 +0530

    Time: 51 ms (50.73%), Space: 57.9 MB (14.13%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -2,22 +2,20 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function (init) {
-   let current = init;
-    return {
-        increment: function () {
-            current += 1
-            return current
-        },
-        decrement: function () {
-            current -= 1
-            return current
-        },
-        reset: function () {
-            current = init
-            return current
-        }
+var createCounter = function(init) {
+  let curnum = init
+  return{
+    increment:()=>{
+        return ++curnum
+    },
+    decrement:()=>{
+        return --curnum
+    },
+    reset:()=>{
+        curnum=init
+        return curnum
     }
+  }  
 };
 
 /**
